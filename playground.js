59ecfb42563a8f9a49ab6a03c390850cff73db5d
Sergio Zanucci <sergiozanu@gmail.com>
2022-06-11 17:54:32 -0300

    teste

---
+++ playground.js
@@ -0,0 +1,21 @@
+/* Map com this */
+
+const mamao = {
+    value: 2,
+}
+
+const melancia = {
+    value: 3,
+}
+
+function MapComThis(arry, thisArg) {
+    return arry.map(function(item){
+        return item * this. value;
+    }, thisArg);
+}
+
+const nums = [1, 2];
+
+console.log('this -> mamão', MapComThis(nums, mamao));
+
+console.log('this -> melancia', MapComThis(nums, melancia));
